b886b39c6a406fa27965d46c17baa6ab3a3e067a
iliashubiitdze <shubitidzeilia7@gmail.com>
2025-01-14 18:57:23 +0400

    Add project files.

---
+++ Gurmany.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Gurmany.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gurmany.Services.CouponAPI.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base (options)
+        {
+            
+        }
+
+        DbSet<Coupon> Coupons {  get; set; }
+    }
+
+    
+}
---
+++ Gurmany.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gurmany.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        [Key]
+        public int CouponId { get; set; }
+
+        [Required]
+        public string CouponCode { get; set; }
+
+        [Required]
+        public Double DiscountAmount { get; set; }
+
+        public int MinAmount { get; set; }
+    }
+}
