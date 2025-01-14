34ad8a471d3782437d99eeb236ea73672ff670ee
iliashubiitdze <shubitidzeilia7@gmail.com>
2025-01-14 22:15:52 +0400

    SeedDb

--- Gurmany.Services.CouponAPI/Migrations/20250114151500_SeedCouponTable.Designer.cs
+++ Gurmany.Services.CouponAPI/Migrations/20250114151500_SeedCouponTable.Designer.cs
@@ -3,6 +3,7 @@ using Gurmany.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gurmany.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250114151500_SeedCouponTable")]
+    partial class SeedCouponTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
