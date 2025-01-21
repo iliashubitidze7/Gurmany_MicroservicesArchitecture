04be0c5c8b98f55eca44c26bf00e7aff5b1b451a
iliashubiitdze <shubitidzeilia7@gmail.com>
2025-01-21 22:58:07 +0400

    Adding IBaseService And BaseService for Sending Request and Getting Response

---
+++ Gurmany.Web/Services/BaseService.cs
@@ -0,0 +1,86 @@
+﻿using Gurmany.Web.Models.DTO;
+using Gurmany.Web.Services.IServices;
+using Gurmany.Web.Utility;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using static Gurmany.Web.Utility.SD;
+
+namespace Gurmany.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+
+                HttpClient client = _httpClient.CreateClient();
+
+                HttpRequestMessage message = new HttpRequestMessage();
+
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(requestDto.Url);
+
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "Application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+
+                switch (apiResponse.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return new ResponseDto { IsSuccess = false, Message = "Not Found" };
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new ResponseDto { IsSuccess = false, Message = "Access Denied" };
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        return new ResponseDto { IsSuccess = false, Message = "Unauthorized" };
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        return new ResponseDto { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponsDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponsDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    IsSuccess = false,
+                    Message = ex.Message.ToString(),
+                };
+                return dto;
+            }
+        }
+    }
+}
---
+++ Gurmany.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Gurmany.Web.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gurmany.Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
